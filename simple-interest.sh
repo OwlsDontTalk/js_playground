02bd1835633a909d732d7922e6064af2c5c5ddfb
Ivan Miropolskii <miropolskiy.ivan@gmail.com>
2025-12-10 20:14:17 -0600

    tmp: add files to pass IBM fullstack assignment requirements

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter principal:"
+read p
+
+echo "Enter rate of interest:"
+read r
+
+echo "Enter time period:"
+read t
+
+si=$(echo "$p * $r * $t / 100" | bc)
+
+echo "Simple Interest = $si"
