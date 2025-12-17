95ae8ccaf59a0b3b7282546d168d273790ecc09e
Ivan Miropolskii <miropolskiy.ivan@gmail.com>
2025-12-17 01:57:56 -0600

    fix: images, count positioning

--- edu/ibm-fullstack-developer/m6/paradise-nursery/src/data/plants.js
+++ edu/ibm-fullstack-developer/m6/paradise-nursery/src/data/plants.js
@@ -4,56 +4,56 @@ const plants = [
     name: 'Monstera Deliciosa',
     price: 34.99,
     category: 'Tropical Favorites',
-    image: 'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1585598117791-876ce25c1884?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-bird',
     name: 'Bird of Paradise',
     price: 42.5,
     category: 'Tropical Favorites',
-    image: 'https://images.unsplash.com/photo-1523345863768-116291ac6624?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1633746562082-98a86ad8b745?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-calathea',
     name: 'Calathea Orbifolia',
     price: 36.25,
     category: 'Tropical Favorites',
-    image: 'https://images.unsplash.com/photo-1522219406764-28c075b2d25f?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1714507767656-2fb307e1b03d?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-snake',
     name: 'Snake Plant',
     price: 28.0,
     category: 'Low Light Lovers',
-    image: 'https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1687552212914-03a30c82053c?q=80&w=830&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-zz',
     name: 'ZZ Plant',
     price: 31.75,
     category: 'Low Light Lovers',
-    image: 'https://images.unsplash.com/photo-1582582494700-321ea7c227cd?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1622673037877-18ee56d1f990?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-pothos',
     name: 'Golden Pothos',
     price: 18.75,
     category: 'Low Light Lovers',
-    image: 'https://images.unsplash.com/photo-1592201426551-02bd6b846c90?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1660756723808-d85024a7e5d4?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-string-pearls',
     name: 'String of Pearls',
     price: 24.5,
     category: 'Succulents & Cacti',
-    image: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1595313356207-52d15f1a35be?q=80&w=776&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 'plant-aloe',
     name: 'Aloe Vera',
     price: 19.95,
     category: 'Succulents & Cacti',
-    image: 'https://images.unsplash.com/photo-1459666636422-224b4f1abe94?auto=format&fit=crop&w=800&q=80'
+    image: 'https://images.unsplash.com/photo-1632380211596-b96123618ca8?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80'
   }
 ];
 
